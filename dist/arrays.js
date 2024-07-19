231f22ddeb06e1506bd9fe429ece8fac9e7ebb79
Alex <alex_soares_oliveira@hotmail.com>
2024-07-19 19:17:35 -0300

    primeira parte

---
+++ dist/arrays.js
@@ -0,0 +1,6 @@
+"use strict";
+
+var redesSociais = ['Facebook', 'Instagram', 'Twitter'];
+for (var i = 0; i < redesSociais.length; i++) {
+  console.log("Eu tenho perfil na rede social: ".concat(redesSociais[i]));
+}
---
+++ src/arrays.js
@@ -0,0 +1,5 @@
+const redesSociais = ['Facebook', 'Instagram', 'Twitter'];
+
+for (let i = 0 ; i < redesSociais.length; i++){
+    console.log(`Eu tenho perfil na rede social: ${redesSociais[i]}`)
+}
